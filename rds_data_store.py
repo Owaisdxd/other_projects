6521bb9fa07802c373a1dfe7f6b9546c9da10530
owais_ahmed <owaisdxd@gmail.com>
2026-07-02 06:27:55 +0400

    Added Mysql data connection

--- rds_data_store.py
+++ rds_data_store.py
@@ -10,3 +10,35 @@ driver.get("https://practicetestautomation.com/practice-test-login/")
 
 time.sleep(5)
 
+input_name = driver.find_element(By.ID,"username")
+input_name.send_keys("student")
+input_passwd = driver.find_element(By.ID,"password")
+input_passwd.send_keys("Password123")
+submit_me = driver.find_element(By.ID,"submit")
+submit_me.click()
+time.sleep(10)
+driver.quit()
+
+conn = pymysql.connect(
+        host='database_identifier',
+        user='admin',
+        password='Admin123',
+        database='my_test_db'
+)
+cursor = conn.cursor()
+
+createl_table_query = """
+       CREATE TABLE IF NOT EXISTS form_submissions (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            username VARCHAR(255),
+            password VARCHAR(255)
+          )
+"""
+cursor.execute(createl_table_query)
+
+sql = "INSERT INTO form_submissions (username, password) VALUES (%s, %s)"
+values = ("student", "admin123")
+cursor.execute(sql, values)
+conn.commit()
+conn.close()
+
