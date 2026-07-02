fd6f3dd3b44547a75c747432e3dd0be034f62e1b
owais_ahmed <owaisdxd@gmail.com>
2026-07-02 08:49:33 +0400

    Failed multiple times but worked atlast

--- sel_test.py
+++ sel_test.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import time
 
-driver=webdriver.Chrome()
+chrome_options = Options()
+chrome_options.add_argument("--headless=new")  # Runs Chrome without a GUI
+chrome_options.add_argument("--no-sandbox")     # Bypasses OS security model (required for Jenkins)
+
+driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://aws.amazon.com/")
+print(driver.title)
 time.sleep(5)
 driver.quit()
 
